42df4294c3edfb34b1d194401f7e451a9587f33f
Fael wild <83206923+fael-wild@users.noreply.github.com>
2022-12-02 23:48:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://fael-wild: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@github.com/fael-wild/mogenius ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 YukkiMusicBot-wildmusic &")
